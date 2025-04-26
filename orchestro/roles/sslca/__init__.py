6e8412281f80888f857a06d06740e26d588027fa
Robert Harris <30379153+enrh@users.noreply.github.com>
2025-04-26 05:17:59 +0000

    Add the commonly defined overview task

---
+++ orchestro/roles/sslca/__init__.py
@@ -0,0 +1,14 @@
+"""
+Functions and routines associated with Enasis Network Orchestrations.
+
+This file is part of Enasis Network software eco-system. Distribution
+is permitted, for more information consult the project license file.
+"""
+
+
+
+from .action_plugins.sslca_params import RoleParams
+
+
+
+__all__ = ['RoleParams']
