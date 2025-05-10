7f4d83ff892d3bcef070494104273e889c6f4e1d
Robert Harris <30379153+enrh@users.noreply.github.com>
2025-05-10 04:32:43 +0000

    First attempt at collection and molecule

---
+++ collections/common/plugins/filter/override.py
@@ -0,0 +1,73 @@
+"""
+Functions and routines associated with Enasis Network Orchestrations.
+
+This file is part of Enasis Network software eco-system. Distribution
+is permitted, for more information consult the project license file.
+"""
+
+
+
+from pathlib import Path
+
+from encommon.parse.jinja2 import FILTERS
+
+
+
+class FilterModule:
+    """
+    Define filter functions available with Ansible routines.
+    """
+
+
+    def filters(
+        # NOCVR
+        self,
+    ) -> FILTERS:
+        """
+        Return the filter functions for use in Ansible routines.
+
+        :returns: Filter functions for use in Ansible routines.
+        """
+
+        return {'basename': basename}
+
+
+
+def basename(
+    path: Path | str,
+    parents: int = 0,
+) -> str:
+    """
+    Return the file name optionally including their parents.
+
+    :param path: Complete or relative path for processing.
+    :param parents: Optional include the additional parents.
+    :returns: File name optionally including their parents.
+    """
+
+    if not path:
+        raise Exception(
+            'There was no path'
+            'given to filter')
+
+    path = Path(path)
+
+    parts = list(
+        path.parts)
+
+    parts = [
+        x for x in parts
+        if x and x != '/']
+
+    if not parts:
+        raise Exception(
+            'There is no file'
+            ' within the path')
+
+    count = min(
+        parents + 1,
+        len(parts))
+
+    result = parts[-count:]
+
+    return str(Path(*result))
